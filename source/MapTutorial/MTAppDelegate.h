b30f0e2d64f8ec998dbed59806a2ca29f1bd3503
ktiniatros <giorgos@giorgos.nl>
2013-06-21 00:30:08 +0200

    source files separation. now. really.

---
+++ source/MapTutorial/MTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTAppDelegate.h
+//  MapTutorial
+//
+//  Created by Giorgos Papadopoulos on 6/21/13.
+//  Copyright (c) 2013 Giorgos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ source/MapTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MapTutorial
+//
+//  Created by Giorgos Papadopoulos on 6/21/13.
+//  Copyright (c) 2013 Giorgos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MTAppDelegate class]));
+    }
+}
